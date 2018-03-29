a4d3f9153c602ed20555e8eadb3cba4b43496de0
ahmed.shaker <ahmed.shaker1707@gmail.com>
2018-03-29 03:47:45 +0200

    localize login & register

--- src/app/auth/auth.module/auth.module.ts
+++ src/app/auth/auth.module/auth.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslateModule} from 'ng2-translate';
 import { AuthRouterModule } from '../auth-route/auth-router.module';
 import { LoginComponent } from '../login/login.component';
 import { RegisterComponent } from '../register/register.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
 
@@ -11,7 +13,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     CommonModule,
     AuthRouterModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    TranslateModule
   ],
   declarations: [
     LoginComponent,
